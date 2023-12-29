31285f537f621fdbc37fd04a2546ca50d72e0368
Admaxx1 <150836124+Admaxx1@users.noreply.github.com>
2023-12-29 08:59:04 -0800

    Update Disk usage.py

--- Disk usage.py
+++ Disk usage.py
@@ -7,7 +7,7 @@ def check():
     space_left = psutil.disk_usage('/')
     print(space_left)
     button.pack_forget()
-    label = Label(wn, text='total space left: ' +str(round(space_left.total / 2**30, 1))+ ' GB\nused space: '+str(round(space_left.used / 2**30, 1))+ ' GB\nfree space: ' +str(round(space_left.free / 2**30, 1)) + ' GB\npercent: '+str(round(space_left.percent / 2**30, 1)) ,font=('comic sans',25))
+    label = Label(wn, text='total space: ' +str(round(space_left.total / 2**30, 1))+ ' GB\nused space: '+str(round(space_left.used / 2**30, 1))+ ' GB\nfree space: ' +str(round(space_left.free / 2**30, 1)) + ' GB\npercent: '+str(round(space_left.percent / 2**30, 1)) ,font=('comic sans',25))
     label.pack()
 
 button = Button(wn,text='Check Space',font=('Arial',20),command=check)
